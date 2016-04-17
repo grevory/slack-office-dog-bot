d0ea3766cc1fb9254734f818c623ab8e13360780
Gregory Pike <greg@gregpike.ca>
2016-04-17 01:12:54 -0230

    Testing Frisket

--- scripts/frisket.js
+++ scripts/frisket.js
@@ -1,6 +1,39 @@
 module.exports = function(robot) {
-  return robot.hear(/frisky/i, function(res) {
-    return res.send("Arf!");
+
+  var _ = require('lodash');
+
+  var map = [
+    {
+      listener: 'hear',
+      response: 'send',
+      pattern: /marco/i,
+      message: 'Yolo!',
+      specificity: 10
+    },
+    {
+      listener: 'hear',
+      response: 'send',
+      pattern: /marco polo/i,
+      message: 'Cool!',
+      specificity: 100
+    }
+  ];
+
+  var bestMatch = null;
+  
+
+  return robot.hear(/(.*)/i, function(res) {
+    
+    var message = res.match[1];
+    var bestMatch = {};
+    _.forEach(map, function(item){
+      if (RegExp(item.pattern).test(message)) {
+        if (item.specificity > bestMatch.specificity) {
+          bestMatch = item;
+        }
+      }
+    });
+
   });
   // robot.hear(/who's a good boy/i, function(res) {
   //   return res.send(":doge:");
