8c9ed1149ac25861fedd15f8292240af75c38cf3
Gregory Pike <greg@gregpike.ca>
2016-05-06 01:46:09 -0230

    Properly hear and respond

--- scripts/frisket.js
+++ scripts/frisket.js
@@ -52,41 +52,51 @@ module.exports = function(robot) {
       specificity: 40
     },
     {
-      listener: 'response',
+      listener: 'respond',
       response: 'reply',
       pattern: 'nap',
       message: ':zzz:',
       specificity: 50
+    },
+    {
+      listener: 'respond',
+      response: 'reply',
+      pattern: 'squirrel',
+      message: 'https://i0.wp.com/www.peacock-panache.com/wp-content/uploads/2014/11/Squirrel.png',
+      specificity: 10
     }
   ];
 
   var bestMatch = null;
-  
 
-  return robot.hear(/(.*)/i, function(res) {
+  _.forEach(_.uniq(_.map(map, 'listener')), function(listener){
+    robot[listener](/(.*)/i, function(res) {
     
-    var post = res.match[1];
-    var bestMatch = {};
-    var message = '';
+      var post = res.match[1];
+      var bestMatch = {};
+      var message = '';
 
-    _.forEach(map, function(item){
-      if (RegExp(item.pattern).test(post)) {
-        if (item.specificity > _.toInteger(bestMatch.specificity)) {
-          bestMatch = item;
+      _.forEach(_.filter(map, function(item){ return item.listener === listener; }), function(item){
+        if (RegExp(item.pattern).test(post)) {
+          if (item.specificity > _.toInteger(bestMatch.specificity)) {
+            bestMatch = item;
+          }
         }
+      });
+
+      if (!_.isEmpty(bestMatch)) {
+        message = bestMatch.message;
+        if (_.isArray(message)) {
+          message = _.sample(message);
+        }
+        if (message.indexOf('%s') >= 0) {
+          message = message.replace('%s', post.match(bestMatch.pattern)[0]);
+        }
+        return res[bestMatch.response](message);
       }
+
     });
-
-    if (!_.isEmpty(bestMatch)) {
-      message = bestMatch.message;
-      if (_.isArray(message)) {
-        message = _.sample(message);
-      }
-      if (message.indexOf('%s') >= 0) {
-        message = message.replace('%s', post.match(bestMatch.pattern)[0]);
-      }
-      return res[bestMatch.response](message);
-    }
-
   });
+
+  return robot;
 };
